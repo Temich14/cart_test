3afd912d2d0151f7a898ba63fbdf13a56c4136c3
Temich-Kawaii-Nya <temich-kawaii-nya@mail.ru>
2025-05-03 20:01:31 +0700

    create order

---
+++ internal/delivery/http/handler/order/handler.go
@@ -0,0 +1,17 @@
+package order
+
+import (
+	"github.com/Temich14/cart_test/internal/service/order"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	s *order.Service
+}
+
+func NewHandler(s *order.Service) *Handler {
+	return &Handler{s: s}
+}
+func (h *Handler) Register(api *gin.RouterGroup) {
+	api.POST("", h.Create)
+}
