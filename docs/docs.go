52a933e154ffa6badd21943dfd8c6f6b594ff630
Temich-Kawaii-Nya <temich-kawaii-nya@mail.ru>
2025-05-11 11:52:35 +0700

    middlewares

--- docs/docs.go
+++ docs/docs.go
@@ -617,13 +617,20 @@ const docTemplate = `{
                 }
             }
         }
+    },
+    "securityDefinitions": {
+        "BearerAuth": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header"
+        }
     }
 }`
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Cart API",
